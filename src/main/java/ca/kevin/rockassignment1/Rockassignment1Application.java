9166628f471f1e62eb9ccaa1de3a19f378284bca
Kevin Tran <tran94@sheridancollege.ca>
2021-02-02 20:50:12 -0500

    Created repository with files

---
+++ src/main/java/ca/kevin/rockassignment1/Rockassignment1Application.java
@@ -0,0 +1,13 @@
+package ca.kevin.rockassignment1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rockassignment1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Rockassignment1Application.class, args);
+    }
+
+}
---
+++ src/test/java/ca/kevin/rockassignment1/Rockassignment1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.kevin.rockassignment1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Rockassignment1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
